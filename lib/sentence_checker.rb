b295235aaf783a60487dbaa7defc994dd173c0c4
josh-p-git <joshua.penney91@gmail.com>
2022-12-06 16:24:47 +0000

    sentence checker update

--- lib/sentence_checker.rb
+++ lib/sentence_checker.rb
@@ -1,3 +1,3 @@
 def sentence_checker(str)
-str[0].upcase == str[0] && str[-1] == "." || str[-1] == "?" || str[-1] == "!"
+    str[0].upcase == str[0] && str[-1] == "." || str[-1] == "?" || str[-1] == "!"
 end
